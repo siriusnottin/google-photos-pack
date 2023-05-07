087990b60713505ba3c84e9131ced1fcc55f2864
Sirius Nottin <30043962+siriusnottin@users.noreply.github.com>
2023-05-07 15:39:08 +0200

    fix(api): add filters for media items
    This commit adds `DateFilter`, `MediaTypeFilter`, `ContentFilter`, and `Filters` to the `MediaItems` module. These filters enable users to filter media items.

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,25 @@
 import * as coda from "@codahq/packs-sdk";
+
 import { Transport } from "./transport";
+
 import { Albums } from "./albums";
 import { MediaItems } from "./media-items";
+
+import { DateFilter } from "./common/date-filter";
+import { MediaTypeFilter } from "./media-items/media-type-filter";
+import { ContentFilter } from "./media-items/content-filter";
+import { Filters } from "./media-items/filters";
 export default class GPhotos {
 
   public readonly transport: Transport;
+
   public readonly albums: Albums;
   public readonly mediaItems: MediaItems;
+
+  public readonly DateFilter = DateFilter;
+  public readonly MediaTypeFilter = MediaTypeFilter;
+  public readonly ContentFilter = ContentFilter;
+  public readonly Filters = Filters;
 
   constructor(public readonly context: coda.ExecutionContext) {
     this.transport = new Transport(context);
